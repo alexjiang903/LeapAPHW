8c5b1480e8c8729ea0940308e2f69fa3730b5d13
Alex J <94213401+alexjiang903@users.noreply.github.com>
2025-02-21 02:03:58 -0500

    attempt to get parser to click on magnifying icon after fully load

--- scraper.js
+++ scraper.js
@@ -11,7 +11,7 @@ async function runWebScraper() {
     try {
         const browser = await puppeteer.launch({
             headless: false,  
-            slowMo: 15, //set to 15 for testing purposes, 75 by default       
+            slowMo: 10, //set to 10 for testing purposes, 75 by default       
             defaultViewport: null 
         });
         const page = await browser.newPage();
@@ -34,7 +34,32 @@ async function runWebScraper() {
         await page.click('a[href="/invoices/all"]');
 
         console.log("succesful navtivation to Invoices -> All"); 
+        
+        //Search #123 in the invoice number search bar
 
+        await page.waitForSelector('th:nth-of-type(9) input', { timeout: 10000});
+        await page.type('th:nth-of-type(9) input', '123', { delay: 150 });
+        await page.keyboard.press('Enter');
+
+        console.log("Searching for invoice 123444...");
+
+        await page.waitForSelector('table#grid', { timeout: 10000 }); //wait for table to load using table's id grid
+        console.log("search results loaded");
+        
+        const invoiceRow = await page.waitForSelector('::-p-xpath(//tr[td[contains(text(), "123444")]])', { timeout: 10000 });
+        console.log(invoiceRow);
+
+        const test = await page.waitForSelector('a[title="View/Edit"]', { timeout: 3000 });
+        if (test) {
+            const html = await page.evaluate(el => el.outerHTML, test);
+            console.log(html);
+            
+        }
+
+        
+        await page.click('a[title="View/Edit"]');
+        
+        console.log("Navigated to invoice 123444 details page");
         //await browser.close();
         //console.log("Scraping finished and PDF saved to local directory.");
     }
